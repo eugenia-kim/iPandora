d082698f2efd5870e242ce1c92fb928de31d08e9
Eugenia Kim <ek2213@ic.ac.uk>
2017-04-03 16:52:56 +0900

    Implement visitDisjuction and visitConjunction and modify visitImplication

--- theorem_prover/fol.py
+++ theorem_prover/fol.py
@@ -26,9 +26,25 @@ class folPrinter(folVisitor):
         return self.visit(ctx.condition())
 
     def visitImplication(self, ctx:folParser.ImplicationContext):
-        disjunctionList = map((lambda d: self.visitDisjunction(d)), ctx.disjunction())
-        return reduce((lambda a, b: "Implies(" + a + ", " + b + ")"), disjunctionList)
+        if not ctx.IMPLIES():
+            return self.visit(ctx.implies())
+        else:
+            disjunctionList = map((lambda d: self.visitDisjunction(d)), ctx.disjunction())
+            return reduce((lambda a, b: "Implies(" + a + ", " + b + ")"), disjunctionList)
 
+    def visitDisjunction(self, ctx:folParser.DisjunctionContext):
+        if not ctx.OR():
+            return self.visitChildren(ctx)
+        else:
+            conjunctionList = map((lambda c: self.visitConjunction(c)), ctx.conjunction())
+            return "Or(" + reduce((lambda a, b: a + ", " + b), conjunctionList) + ")"
+
+    def visitConjunction(self, ctx:folParser.ConjunctionContext):
+        if not ctx.AND():
+            return self.visitChildren(ctx)
+        else:
+            negationList = map((lambda n: self.visitNegation(n)), ctx.negation())
+            return "And(" + reduce((lambda a, b: a + ", " + b), negationList) + ")"
 
 def main(argv):
     input = FileStream(argv[1])
