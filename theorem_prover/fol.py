a12779f718fc3917a95a14bfde2669c8764d0f29
Eugenia Kim <ek2213@ic.ac.uk>
2017-04-03 19:56:04 +0900

    Disjunctions within disjunction ERROR with folImp2 file.

--- theorem_prover/fol.py
+++ theorem_prover/fol.py
@@ -12,6 +12,15 @@ class folPrinter(folVisitor):
     def visitStep(self, ctx: folParser.StepContext):
         return self.visit(ctx.intermediate())
 
+    def visitFormula(self, ctx:folParser.FormulaContext):
+        children = self.visitChildren(ctx)
+        if ctx.VARIABLE() is None:
+            return children
+        elif ctx.FORALL():
+            return "ForAll(" + ctx.VARIABLE().getText()[1:] + ", " + children + ")"
+        else:
+            return "Exists(" + ctx.VARIABLE().getText()[1:] + ", " + children + ")"
+
     def visitNegation(self, ctx:folParser.NegationContext):
         if ctx.NOT() is None:
             return self.visitChildren(ctx)
@@ -20,31 +29,52 @@ class folPrinter(folVisitor):
             return "Not(" + children + ")"
 
     def visitPredicate(self, ctx:folParser.PredicateContext):
-        return ctx.PREPOSITION().getText()
+        children = self.visitChildren(ctx)
+        if children:
+            return ctx.PREPOSITION().getText() + children
+        else:
+            return ctx.PREPOSITION().getText()
+
+    def visitPredicateTuple(self, ctx:folParser.PredicateTupleContext):
+        tuple_list = map((lambda t: self.visitTerm(t)), ctx.term())
+        return "(" + self.__list_to_string(tuple_list) + ")"
+
+    def visitTerm(self, ctx:folParser.TermContext):
+        # TODO: function case
+        return ctx.VARIABLE().getText()[1:]
 
     def visitIntermediate(self, ctx:folParser.IntermediateContext):
         return self.visit(ctx.condition())
 
     def visitImplication(self, ctx:folParser.ImplicationContext):
         if not ctx.IMPLIES():
-            return self.visit(ctx.implies())
-        else:
-            disjunctionList = map((lambda d: self.visitDisjunction(d)), ctx.disjunction())
-            return reduce((lambda a, b: "Implies(" + a + ", " + b + ")"), disjunctionList)
-
-    def visitDisjunction(self, ctx:folParser.DisjunctionContext):
-        if not ctx.OR():
             return self.visitChildren(ctx)
         else:
-            conjunctionList = map((lambda c: self.visitConjunction(c)), ctx.conjunction())
-            return "Or(" + reduce((lambda a, b: a + ", " + b), conjunctionList) + ")"
+            disjunction_list = map((lambda d: self.visitDisjunction(d)), ctx.disjunction())
+            return reduce((lambda a, b: "Implies(" + a + ", " + b + ")"), disjunction_list)
+
+    def visitDisjunction(self, ctx:folParser.DisjunctionContext):
+        print("visiting Disjunction!!!")
+        if not ctx.OR():
+            print("without OR")
+            children = self.visitChildren(ctx)
+            print(children)
+            return children
+        else:
+            print("OR")
+            conjunction_list = map((lambda c: self.visitConjunction(c)), ctx.conjunction())
+            print(self.__list_to_string(conjunction_list))
+            return "Or(" + self.__list_to_string(conjunction_list) + ")"
 
     def visitConjunction(self, ctx:folParser.ConjunctionContext):
         if not ctx.AND():
             return self.visitChildren(ctx)
         else:
-            negationList = map((lambda n: self.visitNegation(n)), ctx.negation())
-            return "And(" + reduce((lambda a, b: a + ", " + b), negationList) + ")"
+            negation_list = map((lambda n: self.visitNegation(n)), ctx.negation())
+            return "And(" + self.__list_to_string(negation_list) + ")"
+
+    def __list_to_string(self, list):
+        return ', '.join(map(str, list))
 
 def main(argv):
     input = FileStream(argv[1])
--- theorem_prover/folParser.py
+++ theorem_prover/folParser.py
@@ -6,43 +6,48 @@ from io import StringIO
 def serializedATN():
     with StringIO() as buf:
         buf.write("\3\u0430\ud6d1\u8206\uad2d\u4417\uaef1\u8d80\uaadd\3\22")
-        buf.write("|\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7\4\b")
-        buf.write("\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r\4\16\t")
-        buf.write("\16\4\17\t\17\3\2\3\2\3\2\3\2\3\3\3\3\3\3\3\4\3\4\3\4")
-        buf.write("\7\4)\n\4\f\4\16\4,\13\4\3\5\3\5\3\6\3\6\5\6\62\n\6\3")
-        buf.write("\6\3\6\3\7\3\7\3\7\7\79\n\7\f\7\16\7<\13\7\3\b\3\b\3\b")
-        buf.write("\7\bA\n\b\f\b\16\bD\13\b\3\t\3\t\3\t\7\tI\n\t\f\t\16\t")
-        buf.write("L\13\t\3\n\5\nO\n\n\3\n\3\n\3\n\3\n\3\n\5\nV\n\n\3\13")
-        buf.write("\3\13\3\13\5\13[\n\13\3\f\3\f\3\f\3\f\7\fa\n\f\f\f\16")
-        buf.write("\fd\13\f\3\f\3\f\3\r\3\r\5\rj\n\r\3\16\3\16\3\16\5\16")
-        buf.write("o\n\16\3\17\3\17\3\17\3\17\7\17u\n\17\f\17\16\17x\13\17")
-        buf.write("\3\17\3\17\3\17\2\2\20\2\4\6\b\n\f\16\20\22\24\26\30\32")
-        buf.write("\34\2\5\3\2\16\20\3\2\t\n\3\2\13\fy\2\36\3\2\2\2\4\"\3")
-        buf.write("\2\2\2\6%\3\2\2\2\b-\3\2\2\2\n\61\3\2\2\2\f\65\3\2\2\2")
-        buf.write("\16=\3\2\2\2\20E\3\2\2\2\22N\3\2\2\2\24Z\3\2\2\2\26\\")
-        buf.write("\3\2\2\2\30i\3\2\2\2\32n\3\2\2\2\34p\3\2\2\2\36\37\5\4")
-        buf.write("\3\2\37 \5\6\4\2 !\7\2\2\3!\3\3\2\2\2\"#\7\16\2\2#$\5")
-        buf.write("\b\5\2$\5\3\2\2\2%*\t\2\2\2&\'\7\21\2\2\')\t\2\2\2(&\3")
-        buf.write("\2\2\2),\3\2\2\2*(\3\2\2\2*+\3\2\2\2+\7\3\2\2\2,*\3\2")
-        buf.write("\2\2-.\5\n\6\2.\t\3\2\2\2/\60\t\3\2\2\60\62\7\13\2\2\61")
-        buf.write("/\3\2\2\2\61\62\3\2\2\2\62\63\3\2\2\2\63\64\5\f\7\2\64")
-        buf.write("\13\3\2\2\2\65:\5\16\b\2\66\67\7\5\2\2\679\5\16\b\28\66")
-        buf.write("\3\2\2\29<\3\2\2\2:8\3\2\2\2:;\3\2\2\2;\r\3\2\2\2<:\3")
-        buf.write("\2\2\2=B\5\20\t\2>?\7\7\2\2?A\5\20\t\2@>\3\2\2\2AD\3\2")
-        buf.write("\2\2B@\3\2\2\2BC\3\2\2\2C\17\3\2\2\2DB\3\2\2\2EJ\5\22")
-        buf.write("\n\2FG\7\6\2\2GI\5\22\n\2HF\3\2\2\2IL\3\2\2\2JH\3\2\2")
-        buf.write("\2JK\3\2\2\2K\21\3\2\2\2LJ\3\2\2\2MO\7\b\2\2NM\3\2\2\2")
-        buf.write("NO\3\2\2\2OU\3\2\2\2PV\5\24\13\2QR\7\3\2\2RS\5\n\6\2S")
-        buf.write("T\7\4\2\2TV\3\2\2\2UP\3\2\2\2UQ\3\2\2\2V\23\3\2\2\2WX")
-        buf.write("\7\r\2\2X[\5\26\f\2Y[\7\r\2\2ZW\3\2\2\2ZY\3\2\2\2[\25")
-        buf.write("\3\2\2\2\\]\7\3\2\2]b\5\30\r\2^_\7\21\2\2_a\5\30\r\2`")
-        buf.write("^\3\2\2\2ad\3\2\2\2b`\3\2\2\2bc\3\2\2\2ce\3\2\2\2db\3")
-        buf.write("\2\2\2ef\7\4\2\2f\27\3\2\2\2gj\5\32\16\2hj\7\13\2\2ig")
-        buf.write("\3\2\2\2ih\3\2\2\2j\31\3\2\2\2kl\7\f\2\2lo\5\34\17\2m")
-        buf.write("o\7\f\2\2nk\3\2\2\2nm\3\2\2\2o\33\3\2\2\2pq\7\3\2\2qv")
-        buf.write("\t\4\2\2rs\7\21\2\2su\t\4\2\2tr\3\2\2\2ux\3\2\2\2vt\3")
-        buf.write("\2\2\2vw\3\2\2\2wy\3\2\2\2xv\3\2\2\2yz\7\4\2\2z\35\3\2")
-        buf.write("\2\2\16*\61:BJNUZbinv")
+        buf.write("\u0088\4\2\t\2\4\3\t\3\4\4\t\4\4\5\t\5\4\6\t\6\4\7\t\7")
+        buf.write("\4\b\t\b\4\t\t\t\4\n\t\n\4\13\t\13\4\f\t\f\4\r\t\r\4\16")
+        buf.write("\t\16\4\17\t\17\3\2\3\2\3\2\3\2\3\3\3\3\3\3\3\4\3\4\3")
+        buf.write("\4\7\4)\n\4\f\4\16\4,\13\4\3\5\3\5\3\6\3\6\5\6\62\n\6")
+        buf.write("\3\6\3\6\3\7\3\7\3\7\3\7\3\7\5\7;\n\7\3\7\3\7\3\7\3\7")
+        buf.write("\3\7\3\7\5\7C\n\7\7\7E\n\7\f\7\16\7H\13\7\3\b\3\b\3\b")
+        buf.write("\7\bM\n\b\f\b\16\bP\13\b\3\t\3\t\3\t\7\tU\n\t\f\t\16\t")
+        buf.write("X\13\t\3\n\5\n[\n\n\3\n\3\n\3\n\3\n\3\n\5\nb\n\n\3\13")
+        buf.write("\3\13\3\13\5\13g\n\13\3\f\3\f\3\f\3\f\7\fm\n\f\f\f\16")
+        buf.write("\fp\13\f\3\f\3\f\3\r\3\r\5\rv\n\r\3\16\3\16\3\16\5\16")
+        buf.write("{\n\16\3\17\3\17\3\17\3\17\7\17\u0081\n\17\f\17\16\17")
+        buf.write("\u0084\13\17\3\17\3\17\3\17\2\2\20\2\4\6\b\n\f\16\20\22")
+        buf.write("\24\26\30\32\34\2\5\3\2\16\20\3\2\t\n\3\2\13\f\u0087\2")
+        buf.write("\36\3\2\2\2\4\"\3\2\2\2\6%\3\2\2\2\b-\3\2\2\2\n\61\3\2")
+        buf.write("\2\2\f:\3\2\2\2\16I\3\2\2\2\20Q\3\2\2\2\22Z\3\2\2\2\24")
+        buf.write("f\3\2\2\2\26h\3\2\2\2\30u\3\2\2\2\32z\3\2\2\2\34|\3\2")
+        buf.write("\2\2\36\37\5\4\3\2\37 \5\6\4\2 !\7\2\2\3!\3\3\2\2\2\"")
+        buf.write("#\7\16\2\2#$\5\b\5\2$\5\3\2\2\2%*\t\2\2\2&\'\7\21\2\2")
+        buf.write("\')\t\2\2\2(&\3\2\2\2),\3\2\2\2*(\3\2\2\2*+\3\2\2\2+\7")
+        buf.write("\3\2\2\2,*\3\2\2\2-.\5\n\6\2.\t\3\2\2\2/\60\t\3\2\2\60")
+        buf.write("\62\7\13\2\2\61/\3\2\2\2\61\62\3\2\2\2\62\63\3\2\2\2\63")
+        buf.write("\64\5\f\7\2\64\13\3\2\2\2\65;\5\16\b\2\66\67\7\3\2\2\67")
+        buf.write("8\5\n\6\289\7\4\2\29;\3\2\2\2:\65\3\2\2\2:\66\3\2\2\2")
+        buf.write(";F\3\2\2\2<B\7\5\2\2=C\5\16\b\2>?\7\3\2\2?@\5\n\6\2@A")
+        buf.write("\7\4\2\2AC\3\2\2\2B=\3\2\2\2B>\3\2\2\2CE\3\2\2\2D<\3\2")
+        buf.write("\2\2EH\3\2\2\2FD\3\2\2\2FG\3\2\2\2G\r\3\2\2\2HF\3\2\2")
+        buf.write("\2IN\5\20\t\2JK\7\7\2\2KM\5\20\t\2LJ\3\2\2\2MP\3\2\2\2")
+        buf.write("NL\3\2\2\2NO\3\2\2\2O\17\3\2\2\2PN\3\2\2\2QV\5\22\n\2")
+        buf.write("RS\7\6\2\2SU\5\22\n\2TR\3\2\2\2UX\3\2\2\2VT\3\2\2\2VW")
+        buf.write("\3\2\2\2W\21\3\2\2\2XV\3\2\2\2Y[\7\b\2\2ZY\3\2\2\2Z[\3")
+        buf.write("\2\2\2[a\3\2\2\2\\b\5\24\13\2]^\7\3\2\2^_\5\n\6\2_`\7")
+        buf.write("\4\2\2`b\3\2\2\2a\\\3\2\2\2a]\3\2\2\2b\23\3\2\2\2cd\7")
+        buf.write("\r\2\2dg\5\26\f\2eg\7\r\2\2fc\3\2\2\2fe\3\2\2\2g\25\3")
+        buf.write("\2\2\2hi\7\3\2\2in\5\30\r\2jk\7\21\2\2km\5\30\r\2lj\3")
+        buf.write("\2\2\2mp\3\2\2\2nl\3\2\2\2no\3\2\2\2oq\3\2\2\2pn\3\2\2")
+        buf.write("\2qr\7\4\2\2r\27\3\2\2\2sv\5\32\16\2tv\7\13\2\2us\3\2")
+        buf.write("\2\2ut\3\2\2\2v\31\3\2\2\2wx\7\f\2\2x{\5\34\17\2y{\7\f")
+        buf.write("\2\2zw\3\2\2\2zy\3\2\2\2{\33\3\2\2\2|}\7\3\2\2}\u0082")
+        buf.write("\t\4\2\2~\177\7\21\2\2\177\u0081\t\4\2\2\u0080~\3\2\2")
+        buf.write("\2\u0081\u0084\3\2\2\2\u0082\u0080\3\2\2\2\u0082\u0083")
+        buf.write("\3\2\2\2\u0083\u0085\3\2\2\2\u0084\u0082\3\2\2\2\u0085")
+        buf.write("\u0086\7\4\2\2\u0086\35\3\2\2\2\20*\61:BFNVZafnuz\u0082")
         return buf.getvalue()
 
 
@@ -437,6 +442,25 @@ class folParser ( Parser ):
                 return self.getTypedRuleContext(folParser.DisjunctionContext,i)
 
 
+        def LPAREN(self, i:int=None):
+            if i is None:
+                return self.getTokens(folParser.LPAREN)
+            else:
+                return self.getToken(folParser.LPAREN, i)
+
+        def formula(self, i:int=None):
+            if i is None:
+                return self.getTypedRuleContexts(folParser.FormulaContext)
+            else:
+                return self.getTypedRuleContext(folParser.FormulaContext,i)
+
+
+        def RPAREN(self, i:int=None):
+            if i is None:
+                return self.getTokens(folParser.RPAREN)
+            else:
+                return self.getToken(folParser.RPAREN, i)
+
         def IMPLIES(self, i:int=None):
             if i is None:
                 return self.getTokens(folParser.IMPLIES)
@@ -470,17 +494,49 @@ class folParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 51
-            self.disjunction()
             self.state = 56
+            self._errHandler.sync(self)
+            la_ = self._interp.adaptivePredict(self._input,2,self._ctx)
+            if la_ == 1:
+                self.state = 51
+                self.disjunction()
+                pass
+
+            elif la_ == 2:
+                self.state = 52
+                self.match(folParser.LPAREN)
+                self.state = 53
+                self.formula()
+                self.state = 54
+                self.match(folParser.RPAREN)
+                pass
+
+
+            self.state = 68
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while _la==folParser.IMPLIES:
-                self.state = 52
-                self.match(folParser.IMPLIES)
-                self.state = 53
-                self.disjunction()
                 self.state = 58
+                self.match(folParser.IMPLIES)
+                self.state = 64
+                self._errHandler.sync(self)
+                la_ = self._interp.adaptivePredict(self._input,3,self._ctx)
+                if la_ == 1:
+                    self.state = 59
+                    self.disjunction()
+                    pass
+
+                elif la_ == 2:
+                    self.state = 60
+                    self.match(folParser.LPAREN)
+                    self.state = 61
+                    self.formula()
+                    self.state = 62
+                    self.match(folParser.RPAREN)
+                    pass
+
+
+                self.state = 70
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
 
@@ -538,17 +594,17 @@ class folParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 59
+            self.state = 71
             self.conjunction()
-            self.state = 64
+            self.state = 76
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while _la==folParser.OR:
-                self.state = 60
+                self.state = 72
                 self.match(folParser.OR)
-                self.state = 61
+                self.state = 73
                 self.conjunction()
-                self.state = 66
+                self.state = 78
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
 
@@ -606,17 +662,17 @@ class folParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 67
+            self.state = 79
             self.negation()
-            self.state = 72
+            self.state = 84
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while _la==folParser.AND:
-                self.state = 68
+                self.state = 80
                 self.match(folParser.AND)
-                self.state = 69
+                self.state = 81
                 self.negation()
-                self.state = 74
+                self.state = 86
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
 
@@ -678,27 +734,27 @@ class folParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 76
+            self.state = 88
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             if _la==folParser.NOT:
-                self.state = 75
+                self.state = 87
                 self.match(folParser.NOT)
 
 
-            self.state = 83
+            self.state = 95
             self._errHandler.sync(self)
             token = self._input.LA(1)
             if token in [folParser.PREPOSITION]:
-                self.state = 78
+                self.state = 90
                 self.predicate()
                 pass
             elif token in [folParser.LPAREN]:
-                self.state = 79
+                self.state = 91
                 self.match(folParser.LPAREN)
-                self.state = 80
+                self.state = 92
                 self.formula()
-                self.state = 81
+                self.state = 93
                 self.match(folParser.RPAREN)
                 pass
             else:
@@ -750,20 +806,20 @@ class folParser ( Parser ):
         localctx = folParser.PredicateContext(self, self._ctx, self.state)
         self.enterRule(localctx, 18, self.RULE_predicate)
         try:
-            self.state = 88
+            self.state = 100
             self._errHandler.sync(self)
-            la_ = self._interp.adaptivePredict(self._input,7,self._ctx)
+            la_ = self._interp.adaptivePredict(self._input,9,self._ctx)
             if la_ == 1:
                 self.enterOuterAlt(localctx, 1)
-                self.state = 85
+                self.state = 97
                 self.match(folParser.PREPOSITION)
-                self.state = 86
+                self.state = 98
                 self.predicateTuple()
                 pass
 
             elif la_ == 2:
                 self.enterOuterAlt(localctx, 2)
-                self.state = 87
+                self.state = 99
                 self.match(folParser.PREPOSITION)
                 pass
 
@@ -822,23 +878,23 @@ class folParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 90
+            self.state = 102
             self.match(folParser.LPAREN)
-            self.state = 91
+            self.state = 103
             self.term()
-            self.state = 96
+            self.state = 108
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while _la==folParser.SEP:
-                self.state = 92
+                self.state = 104
                 self.match(folParser.SEP)
-                self.state = 93
+                self.state = 105
                 self.term()
-                self.state = 98
+                self.state = 110
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
 
-            self.state = 99
+            self.state = 111
             self.match(folParser.RPAREN)
         except RecognitionException as re:
             localctx.exception = re
@@ -886,17 +942,17 @@ class folParser ( Parser ):
         localctx = folParser.TermContext(self, self._ctx, self.state)
         self.enterRule(localctx, 22, self.RULE_term)
         try:
-            self.state = 103
+            self.state = 115
             self._errHandler.sync(self)
             token = self._input.LA(1)
             if token in [folParser.CONSTANT]:
                 self.enterOuterAlt(localctx, 1)
-                self.state = 101
+                self.state = 113
                 self.function()
                 pass
             elif token in [folParser.VARIABLE]:
                 self.enterOuterAlt(localctx, 2)
-                self.state = 102
+                self.state = 114
                 self.match(folParser.VARIABLE)
                 pass
             else:
@@ -948,20 +1004,20 @@ class folParser ( Parser ):
         localctx = folParser.FunctionContext(self, self._ctx, self.state)
         self.enterRule(localctx, 24, self.RULE_function)
         try:
-            self.state = 108
+            self.state = 120
             self._errHandler.sync(self)
-            la_ = self._interp.adaptivePredict(self._input,10,self._ctx)
+            la_ = self._interp.adaptivePredict(self._input,12,self._ctx)
             if la_ == 1:
                 self.enterOuterAlt(localctx, 1)
-                self.state = 105
+                self.state = 117
                 self.match(folParser.CONSTANT)
-                self.state = 106
+                self.state = 118
                 self.functionTuple()
                 pass
 
             elif la_ == 2:
                 self.enterOuterAlt(localctx, 2)
-                self.state = 107
+                self.state = 119
                 self.match(folParser.CONSTANT)
                 pass
 
@@ -1025,33 +1081,33 @@ class folParser ( Parser ):
         self._la = 0 # Token type
         try:
             self.enterOuterAlt(localctx, 1)
-            self.state = 110
+            self.state = 122
             self.match(folParser.LPAREN)
-            self.state = 111
+            self.state = 123
             _la = self._input.LA(1)
             if not(_la==folParser.VARIABLE or _la==folParser.CONSTANT):
                 self._errHandler.recoverInline(self)
             else:
                 self._errHandler.reportMatch(self)
                 self.consume()
-            self.state = 116
+            self.state = 128
             self._errHandler.sync(self)
             _la = self._input.LA(1)
             while _la==folParser.SEP:
-                self.state = 112
+                self.state = 124
                 self.match(folParser.SEP)
-                self.state = 113
+                self.state = 125
                 _la = self._input.LA(1)
                 if not(_la==folParser.VARIABLE or _la==folParser.CONSTANT):
                     self._errHandler.recoverInline(self)
                 else:
                     self._errHandler.reportMatch(self)
                     self.consume()
-                self.state = 118
+                self.state = 130
                 self._errHandler.sync(self)
                 _la = self._input.LA(1)
 
-            self.state = 119
+            self.state = 131
             self.match(folParser.RPAREN)
         except RecognitionException as re:
             localctx.exception = re
