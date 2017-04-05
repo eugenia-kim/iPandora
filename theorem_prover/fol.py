17be88da8cc4a905c5bd647d221958b5255255c1
Eugenia Kim <ek2213@ic.ac.uk>
2017-04-05 19:34:30 +0900

    folNeg bug fixed. folImp2 still not passing

--- theorem_prover/fol.py
+++ theorem_prover/fol.py
@@ -7,13 +7,12 @@ from folVisitor import folVisitor
 from functools import *
 
 class folPrinter(folVisitor):
-
     # Visit a parse tree produced by folParser#step.
     def visitStep(self, ctx: folParser.StepContext):
         return self.visit(ctx.intermediate())
 
-    def visitFormula(self, ctx:folParser.FormulaContext):
-        children = self.visitChildren(ctx)
+    def visitFormula(self, ctx: folParser.FormulaContext):
+        children = self.visit(ctx.implication())
         if ctx.VARIABLE() is None:
             return children
         elif ctx.FORALL():
@@ -21,57 +20,64 @@ class folPrinter(folVisitor):
         else:
             return "Exists(" + ctx.VARIABLE().getText()[1:] + ", " + children + ")"
 
-    def visitNegation(self, ctx:folParser.NegationContext):
+    def visitNegation(self, ctx: folParser.NegationContext):
+        print("Negation")
         if ctx.NOT() is None:
+            print("without !")
             return self.visitChildren(ctx)
         else:
+            print("with !")
             children = self.visitChildren(ctx)
             return "Not(" + children + ")"
 
-    def visitPredicate(self, ctx:folParser.PredicateContext):
+    def visitPredicate(self, ctx: folParser.PredicateContext):
         children = self.visitChildren(ctx)
         if children:
             return ctx.PREPOSITION().getText() + children
         else:
             return ctx.PREPOSITION().getText()
 
-    def visitPredicateTuple(self, ctx:folParser.PredicateTupleContext):
-        tuple_list = map((lambda t: self.visitTerm(t)), ctx.term())
-        return "(" + self.__list_to_string(tuple_list) + ")"
+    def visitPredicateTuple(self, ctx: folParser.PredicateTupleContext):
+        tuple_list = map((lambda t: self.visit(t)), ctx.term())
+        return "(" + reduce((lambda a, b: a + ", " +  b), tuple_list) + ")"
 
-    def visitTerm(self, ctx:folParser.TermContext):
+    def visitTerm(self, ctx: folParser.TermContext):
         # TODO: function case
         return ctx.VARIABLE().getText()[1:]
 
-    def visitIntermediate(self, ctx:folParser.IntermediateContext):
+    def visitIntermediate(self, ctx: folParser.IntermediateContext):
+        print("Intermediate")
         return self.visit(ctx.condition())
 
-    def visitImplication(self, ctx:folParser.ImplicationContext):
+    def visitImplication(self, ctx: folParser.ImplicationContext):
+        print("Implication")
         if not ctx.IMPLIES():
-            return self.visitChildren(ctx)
+            print("no ->")
+            return self.visit(ctx.disjunction(0))
         else:
-            disjunction_list = map((lambda d: self.visitDisjunction(d)), ctx.disjunction())
-            return reduce((lambda a, b: "Implies(" + a + ", " + b + ")"), disjunction_list)
+            print("yes ->")
+            disjunctionList = map((lambda d: self.visit(d)), ctx.disjunction())
+            return reduce((lambda a, b: "Implies(" + a + ", " + b + ")"), disjunctionList)
 
-    def visitDisjunction(self, ctx:folParser.DisjunctionContext):
-        print("visiting Disjunction!!!")
+    def visitDisjunction(self, ctx: folParser.DisjunctionContext):
+        print("Disjunction")
         if not ctx.OR():
-            print("without OR")
-            children = self.visitChildren(ctx)
-            print(children)
-            return children
+            print("no |")
+            return self.visit(ctx.conjunction(0))
         else:
-            print("OR")
-            conjunction_list = map((lambda c: self.visitConjunction(c)), ctx.conjunction())
-            print(self.__list_to_string(conjunction_list))
-            return "Or(" + self.__list_to_string(conjunction_list) + ")"
+            print("yes |")
+            conjunctionList = map((lambda c: self.visit(c)), ctx.conjunction())
+            return "Or(" + reduce((lambda a, b: a + ", " + b), conjunctionList) + ")"
 
-    def visitConjunction(self, ctx:folParser.ConjunctionContext):
+    def visitConjunction(self, ctx: folParser.ConjunctionContext):
+        print("Conjunction")
         if not ctx.AND():
-            return self.visitChildren(ctx)
+            print("no &")
+            return self.visit(ctx.negation(0))
         else:
-            negation_list = map((lambda n: self.visitNegation(n)), ctx.negation())
-            return "And(" + self.__list_to_string(negation_list) + ")"
+            print("yes &")
+            negationList = map((lambda n: self.visit(n)), ctx.negation())
+            return "And(" + reduce((lambda a, b: a + ", " + b), negationList) + ")"
 
     def __list_to_string(self, list):
         return ', '.join(map(str, list))
