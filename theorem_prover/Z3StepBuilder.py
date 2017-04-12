5aed88210491912c18d9bc252d3269cb52684074
Eugenia Kim <ek2213@ic.ac.uk>
2017-04-13 07:48:57 +0900

    Error in StepBuilder in line 75. Z3 Exception: Z3 expression expected

--- theorem_prover/Z3StepBuilder.py
+++ theorem_prover/Z3StepBuilder.py
@@ -2,15 +2,25 @@ import sys
 from antlr4 import *
 from folLexer import folLexer
 from folParser import folParser
-#from fol_listener import fol_listener
 from folVisitor import folVisitor
 from functools import *
 from z3 import *
+# TODO: figure out how python's import works
+from Z3TypeBuilder import Z3TypeBuilder
+from folTypeLexer import folTypeLexer
+from folTypeParser import folTypeParser
 
 class Z3StepBuilder(folVisitor):
 
-    def __init__(self):
+    def __init__(self, type_builder):
+        # proposition_map = [proposition_name, z3 Bool]
         self.proposition_map = dict()
+
+        # Z3 Type Builder which has predicate_map and param_map
+        self.type_builder = type_builder
+
+        # term_map = [term_name, z3 sort]
+        self.term_map = dict()
 
     # Visit a parse tree produced by folParser#step.
     def visitStep(self, ctx: folParser.StepContext):
@@ -21,9 +31,13 @@ class Z3StepBuilder(folVisitor):
         if ctx.VARIABLE() is None:
             return children
         elif ctx.FORALL():
-            return "ForAll(" + ctx.VARIABLE().getText()[1:] + ", " + children + ")"
+            term = self.term_map.get(ctx.VARIABLE().getText()[1:])
+            return ForAll(term, children)
+            # return "ForAll(" + ctx.VARIABLE().getText()[1:] + ", " + children + ")"
         else:
-            return "Exists(" + ctx.VARIABLE().getText()[1:] + ", " + children + ")"
+            term = self.term_map.get(ctx.VARIABLE().getText()[1:])
+            return Exists(term, children)
+            #return "Exists(" + ctx.VARIABLE().getText()[1:] + ", " + children + ")"
 
     def visitNegation(self, ctx: folParser.NegationContext):
         print("Negation")
@@ -39,10 +53,28 @@ class Z3StepBuilder(folVisitor):
             return Not(children)
 
     def visitPredicate(self, ctx: folParser.PredicateContext):
+        # TODO: change this to visit(ctx.predicateTuple())
         children = self.visitChildren(ctx)
         if children:
             # Predicate Tuple type
-            return ctx.PREPOSITION().getText() + children
+
+            # get z3 parameter types
+            param_type = self.type_builder.param_map.get(ctx.PREPOSITION().getText())
+
+            # get z3 predicate function
+            predicate = self.type_builder.predicate_map.get(ctx.PREPOSITION().getText())
+
+            # get z3 parameters
+            z3_param = list(map((lambda tuple: Consts(tuple[0], tuple[1])), zip(children, param_type)))
+            print(z3_param)
+            print(Consts('x', IntSort()))
+
+            # add z3 params to term_map
+            map((lambda name, z3_param: self.__add_term_map(name, z3_param)), zip(children, z3_param))
+            print("HHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHHh")
+            print(str(predicate(*z3_param)))
+            return predicate(*z3_param)
+            # return ctx.PREPOSITION().getText() + children
         else:
             # simple predicate Bool type
             proposition = self.proposition_map.get(ctx.PREPOSITION().getText())
@@ -54,8 +86,9 @@ class Z3StepBuilder(folVisitor):
             #return Bool(ctx.PREPOSITION().getText())
 
     def visitPredicateTuple(self, ctx: folParser.PredicateTupleContext):
-        tuple_list = map((lambda t: self.visit(t)), ctx.term())
-        return "(" + reduce((lambda a, b: a + ", " +  b), tuple_list) + ")"
+        tuple_list = list(map((lambda t: self.visit(t)), ctx.term()))
+        return tuple_list
+        #return "(" + reduce((lambda a, b: a + ", " +  b), tuple_list) + ")"
 
     def visitTerm(self, ctx: folParser.TermContext):
         # TODO: function case
@@ -98,18 +131,32 @@ class Z3StepBuilder(folVisitor):
             #return "And(" + reduce((lambda a, b: a + ", " + b), negation_list) + ")"
             return And(*negation_list)
 
+    def __add_term_map(self, name, z3):
+       if self.term_map.get(name) is None:
+           self.term_map[name] = z3
+
     def __list_to_string(self, list):
         return ', '.join(map(str, list))
 
 def main(argv):
-    input = FileStream(argv[1])
-    lexer = folLexer(input)
-    stream = CommonTokenStream(lexer)
-    parser = folParser(stream)
 
-    tree = parser.step()
-    printer = Z3StepBuilder()
-    print(str(printer.visit(tree)))
+    type_input = FileStream(argv[1])
+    type_lexer = folTypeLexer(type_input)
+    type_stream = CommonTokenStream(type_lexer)
+    type_parser = folTypeParser(type_stream)
+
+    type_tree = type_parser.init()
+    type_builder = Z3TypeBuilder()
+    type_builder.visit(type_tree)
+
+    step_input = FileStream(argv[2])
+    step_lexer = folLexer(step_input)
+    step_stream = CommonTokenStream(step_lexer)
+    step_parser = folParser(step_stream)
+
+    step_tree = step_parser.step()
+    step_builder = Z3StepBuilder(type_builder)
+    print(str(step_builder.visit(step_tree)))
 
 if __name__ == '__main__':
     main(sys.argv)
