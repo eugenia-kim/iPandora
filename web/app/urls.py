3aa0a13165b3a1d9186866d46ee99c1f3eb83344
Eugenia Kim <ek2213@ic.ac.uk>
2017-06-12 01:13:36 +0100

    Add swagger

--- web/app/urls.py
+++ web/app/urls.py
@@ -16,8 +16,12 @@ Including another URLconf
 from django.conf.urls import include, url
 from django.contrib import admin
 from django.views.generic import TemplateView
+from rest_framework_swagger.views import get_swagger_view
+
+schema_view = get_swagger_view(title='Pastebin API')
 
 urlpatterns = [
+    url(r'^swagger/$', schema_view),
     url(r'^api/', include('app.api.urls')),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
